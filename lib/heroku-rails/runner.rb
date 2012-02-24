d4948108e130532af1f3b39c973a7262f27fade0
DarkBushido <lance.sanchez+github@gmail.com>
2012-02-24 11:38:23 -0800

    adding in the ronin and shared pg9 to the list of things that stop the shared db from being added

--- lib/heroku-rails/runner.rb
+++ lib/heroku-rails/runner.rb
@@ -153,7 +153,10 @@ module HerokuRails
         existing_addons = (@heroku.installed_addons(app_name) || []).map{|a| a["name"]}
 
         # all apps need the shared database
-        addons << "shared-database:5mb" unless addons.index("shared-database:5mb") || addons.index("shared-database:20gb")
+        addons << "shared-database:5mb" unless  addons.index("shared-database:5mb")     || 
+                                                addons.index("shared-database:20gb")    || 
+                                                addons.index("heroku-postgresql:ronin") ||
+                                                addons.index("heroku-shared-postgresql:basic")
 
         # add "custom_domains" if that addon doesnt already exist
         # and we have domains configured for this app
